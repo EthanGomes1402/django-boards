d8db0c40691c1cad71b2df86dd7267743751945b
EthanGomes1402 <ethangmz1402@gmail.com>
2021-10-07 11:21:34 +0530

    Initial commit

--- djgo1/settings.py
+++ djgo1/settings.py
@@ -84,7 +84,6 @@ WSGI_APPLICATION = 'djgo1.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
